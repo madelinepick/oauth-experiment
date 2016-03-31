bd4b1b326335614a141bf690c886e99eefe08ff6
Madeline Pickering <madelinepickering@gmail.com>
2016-03-31 14:17:37 -0600

    changed callback in passport use

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ passport.use(new LinkedInStrategy({
   clientID: process.env.LINKEDIN_CLIENT_ID,
   clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
   callbackURL: process.env.HOST + "/auth/linkedin/callback",
-  state: true,
   scope: ['r_emailaddress', 'r_basicprofile'],
+  state: true
 }, function(accessToken, refreshToken, profile, done) {
   done(null, {id: profile.id, displayName: profile.displayName})
 }));
